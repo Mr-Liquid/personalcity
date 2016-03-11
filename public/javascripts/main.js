bcc4eaf4e40a5f620d991eb8bfa75099f4ca065c
Ivanov Ivan <Ivanov Ivan>
2016-03-11 16:48:01 +0300

    added fullpage plagin behaviour

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,10 @@
  * Created by 1 on 11/03/2016.
  */
 (function(global,$){
-    $(document).ready(function() {
-        $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        menu: '#menu',
+        scrollingSpeed: 1000
     });
 })(window,jQuery);
